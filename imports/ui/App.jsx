8da06ac348c5a7af7f788b6976dcb778624c192a
Blake Larson <blake7@hawaii.edu>
2016-04-15 19:46:41 -1000

    added more tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
- 
+
 import Task from './Task.jsx';
- 
+
 // App component - represents the whole app
 export default class App extends Component {
   getTasks() {
@@ -9,22 +9,24 @@ export default class App extends Component {
       { _id: 1, text: 'This is task 1' },
       { _id: 2, text: 'This is task 2' },
       { _id: 3, text: 'This is task 3' },
+      { _id: 4, text: 'This is task 4' },
+      { _id: 5, text: 'This is task 5' },
     ];
   }
- 
+
   renderTasks() {
     return this.getTasks().map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
- 
+
   render() {
     return (
       <div className="container">
         <header>
           <h1>Todo List</h1>
         </header>
- 
+
         <ul>
           {this.renderTasks()}
         </ul>
